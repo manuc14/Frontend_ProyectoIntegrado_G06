a2dd7c4150bfac53fa508d34df3a3cf8038b7e89
mariodamas <mariodamassanchez@gmail.com>
2025-11-14 21:15:20 +0100

    Nuevas rutas de backend añadidas y corrección del Deploy Stage en la pipeline

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseApiUrl: 'https://backend-g06-api.onrender.com/api', // URL para APIs
-  baseResourceUrl: 'https://backend-g06-api.onrender.com/resources', // recursos estáticos
+  baseApiUrl: 'https://backend-proyectointegrado-g06.onrender.com/api', // URL para APIs
+  baseResourceUrl: 'https://backend-proyectointegrado-g06.onrender.com/resources', // recursos estáticos
   useMocks: false,
 };
