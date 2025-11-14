c5985fca5bd016183df378b0a3637b2b9de06b6e
mariodamas <mariodamassanchez@gmail.com>
2025-11-14 20:50:05 +0100

    Despliegue en firebase agregado y funcionando

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  baseApiUrl: 'https://localhost:8080/api', // URL para APIs
+  baseApiUrl: 'https://backend-g06-api.onrender.com/api', // URL para APIs
+  baseResourceUrl: 'https://backend-g06-api.onrender.com/resources', // recursos estáticos
   useMocks: false,
 };
